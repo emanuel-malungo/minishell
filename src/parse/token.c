6c1a2e8f04742be7c9495284153ec2c60230e8f8
Emanuel Malungo <emalungo@c1r2s4.42luanda.com>
2024-11-16 12:34:08 +0100

    echo imprimindo varivel de ambiente

--- src/parse/token.c
+++ src/parse/token.c
@@ -6,7 +6,7 @@
 /*   By: emalungo <emalungo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 08:24:11 by emalungo          #+#    #+#             */
-/*   Updated: 2024/11/16 12:31:54 by emalungo         ###   ########.fr       */
+/*   Updated: 2024/11/16 12:33:38 by emalungo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,152 +68,159 @@ void	ft_free(char **strs, int j)
 	free(strs);
 }
 
-char	**ft_token(char const *s)
+char **ft_token(char const *s)
 {
-	char	*expanded;
-	char	var[var_size + 2];
-	char	var[var_size + 2];
+    int i, j, size, var_size;
+    char **strs, quote;
+    char *expanded;
 
-	int i, j, size, var_size;
-	char **strs, quote;
-	i = 0;
-	j = 0;
-	strs = (char **)malloc((ft_count_word(s) + 1) * sizeof(char *));
-	if (!strs)
-		return (NULL);
-	while (s[i])
-	{
-		while (s[i] && isspace(s[i]))
-			i++;
-		if (s[i] == '\"' || s[i] == '\'')
-		{
-			quote = s[i++];
-			size = 0;
-			if (quote == '\"')
-			{
-				while (s[i] && s[i] != quote)
-				{
-					if (s[i] == '$')
-					{
-						var_size = 0;
-						i++;
-						while (s[i] && (isalnum(s[i]) || s[i] == '_'))
-						{
-							var_size++;
-							i++;
-						}
-						strncpy(var, &s[i - var_size - 1], var_size + 1);
-						var[var_size + 1] = '\0';
-						expanded = expand_env_var(var);
-						if (!expanded)
-							expanded = ft_strdup("");
-						strs[j] = expanded;
-						j++;
-					}
-					else
-					{
-						strs[j] = (char *)malloc(2);
-						if (!strs[j])
-						{
-							ft_free(strs, j);
-							return (NULL);
-						}
-						strs[j][0] = s[i];
-						strs[j++][1] = '\0';
-						i++;
-					}
-				}
-			}
-			else
-			{
-				while (s[i] && s[i] != quote)
-				{
-					strs[j] = (char *)malloc(2);
-					if (!strs[j])
-					{
-						ft_free(strs, j);
-						return (NULL);
-					}
-					strs[j][0] = s[i];
-					strs[j++][1] = '\0';
-					i++;
-				}
-			}
-			i++;
-		}
-		else if (s[i] == '$')
-		{
-			var_size = 0;
-			i++;
-			while (s[i] && (isalnum(s[i]) || s[i] == '_'))
-			{
-				var_size++;
-				i++;
-			}
-			strncpy(var, &s[i - var_size - 1], var_size + 1);
-			var[var_size + 1] = '\0';
-			expanded = expand_env_var(var);
-			if (!expanded)
-				expanded = ft_strdup("");
-			strs[j] = expanded;
-			j++;
-		}
-		else if (!isspace(s[i]) && s[i] != '|' && s[i] != '>' && s[i] != '<')
-		{
-			size = ft_wordsize(s, i);
-			strs[j] = (char *)malloc(size + 1);
-			if (!strs[j])
-			{
-				ft_free(strs, j);
-				return (NULL);
-			}
-			strncpy(strs[j], &s[i], size);
-			strs[j++][size] = '\0';
-			i += size;
-		}
-		else if (s[i] == '>' && s[i + 1] == '>')
-		{
-			strs[j] = (char *)malloc(3);
-			if (!strs[j])
-			{
-				ft_free(strs, j);
-				return (NULL);
-			}
-			strs[j][0] = s[i];
-			strs[j][1] = s[i + 1];
-			strs[j++][2] = '\0';
-			i += 2;
-		}
-		else if (s[i] == '<' && s[i + 1] == '<')
-		{
-			strs[j] = (char *)malloc(3);
-			if (!strs[j])
-			{
-				ft_free(strs, j);
-				return (NULL);
-			}
-			strs[j][0] = s[i];
-			strs[j][1] = s[i + 1];
-			strs[j++][2] = '\0';
-			i += 2;
-		}
-		else if (s[i] == '|' || s[i] == '>' || s[i] == '<')
-		{
-			strs[j] = (char *)malloc(2);
-			if (!strs[j])
-			{
-				ft_free(strs, j);
-				return (NULL);
-			}
-			strs[j][0] = s[i];
-			strs[j++][1] = '\0';
-			i++;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	strs[j] = NULL;
-	return (strs);
+    i = 0;
+    j = 0;
+    strs = (char **)malloc((ft_count_word(s) + 1) * sizeof(char *));
+    if (!strs)
+        return (NULL);
+    
+    while (s[i])
+    {
+        while (s[i] && isspace(s[i]))
+            i++;
+        
+        if (s[i] == '\"' || s[i] == '\'')
+        {
+            quote = s[i++];
+            size = 0;
+            
+            if (quote == '\"')
+            {
+                while (s[i] && s[i] != quote)
+                {
+                    if (s[i] == '$')
+                    {
+                        var_size = 0;
+                        i++;
+                        while (s[i] && (isalnum(s[i]) || s[i] == '_'))
+                        {
+                            var_size++;
+                            i++;
+                        }
+                        char var[var_size + 2];
+                        strncpy(var, &s[i - var_size - 1], var_size + 1);
+                        var[var_size + 1] = '\0';
+                        
+
+                        expanded = expand_env_var(var);
+                        if (!expanded)
+                            expanded = ft_strdup("");
+                        
+                        strs[j] = expanded;
+                        j++;
+                    }
+                    else 
+                    {
+                        strs[j] = (char *)malloc(2);
+                        if (!strs[j])
+                        {
+                            ft_free(strs, j);
+                            return (NULL);
+                        }
+                        strs[j][0] = s[i];
+                        strs[j++][1] = '\0';
+                        i++;
+                    }
+                }
+            }
+            else
+            {
+                while (s[i] && s[i] != quote)
+                {
+                    strs[j] = (char *)malloc(2);
+                    if (!strs[j])
+                    {
+                        ft_free(strs, j);
+                        return (NULL);
+                    }
+                    strs[j][0] = s[i];
+                    strs[j++][1] = '\0';
+                    i++;
+                }
+            }
+            i++;
+        }
+        else if (s[i] == '$')
+        {
+            var_size = 0;
+            i++;
+            while (s[i] && (isalnum(s[i]) || s[i] == '_'))
+            {
+                var_size++;
+                i++;
+            }
+            char var[var_size + 2];
+            strncpy(var, &s[i - var_size - 1], var_size + 1);
+            var[var_size + 1] = '\0';
+            expanded = expand_env_var(var);
+            if (!expanded)
+                expanded = ft_strdup("");
+            
+            strs[j] = expanded;
+            j++;
+        }
+        else if (!isspace(s[i]) && s[i] != '|' && s[i] != '>' && s[i] != '<')
+        {
+            size = ft_wordsize(s, i);
+            strs[j] = (char *)malloc(size + 1);
+            if (!strs[j])
+            {
+                ft_free(strs, j);
+                return (NULL);
+            }
+            strncpy(strs[j], &s[i], size);
+            strs[j++][size] = '\0';
+            i += size;
+        }
+        else if (s[i] == '>' && s[i + 1] == '>')
+        {
+            strs[j] = (char *)malloc(3);
+            if (!strs[j])
+            {
+                ft_free(strs, j);
+                return (NULL);
+            }
+            strs[j][0] = s[i];
+            strs[j][1] = s[i + 1];
+            strs[j++][2] = '\0';
+            i += 2;
+        }
+        else if (s[i] == '<' && s[i + 1] == '<')
+        {
+            strs[j] = (char *)malloc(3);
+            if (!strs[j])
+            {
+                ft_free(strs, j);
+                return (NULL);
+            }
+            strs[j][0] = s[i];
+            strs[j][1] = s[i + 1];
+            strs[j++][2] = '\0';
+            i += 2;
+        }
+        else if (s[i] == '|' || s[i] == '>' || s[i] == '<')
+        {
+            strs[j] = (char *)malloc(2);
+            if (!strs[j])
+            {
+                ft_free(strs, j);
+                return (NULL);
+            }
+            strs[j][0] = s[i];
+            strs[j++][1] = '\0';
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    strs[j] = NULL;
+    return (strs);
 }
